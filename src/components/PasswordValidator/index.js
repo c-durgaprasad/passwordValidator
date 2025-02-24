d12204120711336501db562cf79f3c207be40aeb
cdurgaprasad <dprasad.ch10@gmail.com>
2025-02-24 06:03:32 +0000

    Final commit

--- src/components/PasswordValidator/index.js
+++ src/components/PasswordValidator/index.js
@@ -1,1 +1,39 @@
 // Write your code here
+import {useState} from 'react'
+import {
+  BgContainer,
+  CardContainer,
+  PsHeading,
+  PsDesc,
+  PsInput,
+  ErrMsg,
+} from './styledComponents'
+
+const PasswordValidator = () => {
+  const [validate, setValidate] = useState(
+    'Your password must be at least 8 characters',
+  )
+  const [password, setPassword] = useState('')
+
+  const passwordChange = event => {
+    setPassword(event.target.value)
+    if (password.length < 8) {
+      setValidate('Your password must be at least 8 characters')
+    } else {
+      setValidate('')
+    }
+  }
+
+  return (
+    <BgContainer>
+      <CardContainer>
+        <PsHeading>Password Validator</PsHeading>
+        <PsDesc>Check how strong and secure is your password</PsDesc>
+        <PsInput type="password" onChange={passwordChange} value={password} s />
+        <ErrMsg>{validate}</ErrMsg>
+      </CardContainer>
+    </BgContainer>
+  )
+}
+
+export default PasswordValidator
--- src/components/PasswordValidator/styledComponents.js
+++ src/components/PasswordValidator/styledComponents.js
@@ -1,1 +1,70 @@
 // Style your elements here
+import styled from 'styled-components'
+
+export const BgContainer = styled.div`
+  background-color: #24263c;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+export const CardContainer = styled.div`
+  background-color: #475569;
+  width: 80%;
+  border-radius: 10px;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media screen and (min-width: 768px) {
+    width: 400px;
+    height: 300px;
+  }
+`
+export const PsHeading = styled.h1`
+  margin: 0px;
+  font-family: 'Roboto';
+  color: #f8fafc;
+  font-size: 22px;
+  padding-top: 22px;
+  @media screen and (min-width: 768px) {
+    font-size: 32px;
+    font-weight: 500;
+    padding-top: 70px;
+  }
+`
+
+export const PsDesc = styled.p`
+  font-family: 'Roboto';
+  color: #f8fafc;
+  font-size: 14px;
+  text-align: center;
+  @media screen and (min-width: 768px) {
+    font-size: 11px;
+    padding-top: 20px;
+  }
+`
+
+export const PsInput = styled.input`
+  height: 34px;
+  width: 70%;
+  margin-top: 25px;
+  font-family: 'Roboto';
+  outline: none;
+  padding-left: 10px;
+  @media screen and (min-width: 768px) {
+    width: 300px;
+  }
+`
+export const ErrMsg = styled.p`
+  font-family: 'Roboto';
+  color: #ef4444;
+  margin: 0px;
+  font-size: 14px;
+  padding-top: 3px;
+  @media screen and (min-width: 768px) {
+    font-size: 11px;
+    padding-top: 6px;
+  }
+`
